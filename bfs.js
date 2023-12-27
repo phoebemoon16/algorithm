6fbc69aa35507354fe3fa724b1096f5e58a6320e
wanghh5 <wanghh5@baoneng.com>
2023-12-27 16:05:32 +0800

    提交理解说明

--- bfs.js
+++ bfs.js
@@ -2,7 +2,7 @@
  * @Author: wanghh
  * @Date: 2023-12-27 08:23:10
  * @LastEditors: wanghh
- * @LastEditTime: 2023-12-27 15:30:19
+ * @LastEditTime: 2023-12-27 16:01:48
  * @Description: 根据映射图去查找最小路径
  */
 /**广度优先 */
@@ -17,20 +17,24 @@ function recursionRelation(source, traget) {
        * queue.push()
        * queue.shift()
        */
-      const path = queue.shift(); // 取出一个元素
-      const currentStatusTemp = path[path.length - 1];
-      // 1. 输出path 可能是没有相对应的路径
-      if (currentStatusTemp === traget) {
-        return path;
-      }
-      // 2 判断能不能到达 path只有一个节点 也有是可能是自己倒自己 主要注意区分一下
-      //   if (currentStatusTemp === value && tempArr.includes(value)) {
+      console.log(queue, "path00");
+      const path = queue.shift(); // 取出队列的第一个数组元素
+      const currentStatusTemp = path[path.length - 1]; // 取出最后一个元素 就是上一个push的邻居节点
+      // 1. 输出path 可能是没有相对应的路径自己倒自己有点问题
+      //   if (currentStatusTemp === traget) {
       //     return path;
       //   }
+      // 2 判断能不能到达 path只有一个节点 也有是可能是自己倒自己 主要注意区分一下
+      console.log(path, "path11");
+      const tempArr = path.slice(1);
+      if (currentStatusTemp === traget && tempArr.includes(traget)) {
+        return path;
+      }
       if (!visited.has(currentStatusTemp)) {
         visited.add(currentStatusTemp);
         for (const nextStatus of stateMap[currentStatusTemp]) {
-          queue.push([...path, nextStatus]);
+          console.log(nextStatus, "nextStatus");
+          queue.push([...path, nextStatus]); // 当前路径加上邻居节点 分别push进去
         }
       }
     }
@@ -39,15 +43,11 @@ function recursionRelation(source, traget) {
 }
 
 let stateMap = {
-  A: ["A", "B", "C"],
-  B: ["A", "D", "E"],
-  C: ["A", "F", "G"],
-  D: ["B"],
-  E: ["B", "H"],
-  F: ["C"],
-  G: ["C"],
-  H: ["E"],
+  A: ["C", "D"],
+  B: ["B", "D"],
+  C: ["B"],
+  D: ["C", "A"],
 };
 
-console.log(recursionRelation("A", "H"));
+console.log(recursionRelation("D", "D"));
 console.log(recursionRelation("A", "A"));
