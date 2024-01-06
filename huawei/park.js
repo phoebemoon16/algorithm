35b738923464d1ea3866d65f32fafb2e33e045f1
phoebemoon16 <1427828531@qq.com>
2024-01-06 21:11:48 +0800

    提交字符串算法

---
+++ huawei/park.js
@@ -0,0 +1,64 @@
+const { count } = require("console")
+
+/**
+ * 
+ * 题目描述
+特定大小的停车场，数组cars[]表示，其中1表示有车，0表示没车。
+
+车辆大小不一，小车占一个车位（长度1），货车占两个车位（长度2），卡车占三个车位（长度3）。
+
+统计停车场最少可以停多少辆车，返回具体的数目。
+
+输入描述
+整型字符串数组cars[]，其中1表示有车，0表示没车，数组长度小于1000。
+
+输出描述
+整型数字字符串，表示最少停车数目。
+
+用例1
+输入
+1,0,1
+输出
+2
+说明
+1个小车占第1个车位
+第二个车位空
+1个小车占第3个车位
+最少有两辆车
+
+用例2
+输入
+1,1,0,0,1,1,1,0,1
+输出
+3
+说明
+1个货车占第1、2个车位
+第3、4个车位空
+1个卡车占第5、6、7个车位
+第8个车位空
+1个小车占第9个车位
+最少3辆车
+
+解题思路： 把111替换成x  11替换成X 1替换成x 看有多少个x 就是最少有多少辆车
+ */
+function solution(str) {
+    let str2 = str.replaceAll(',', '').replaceAll('111','x').replaceAll('11','x').replaceAll('1','x')
+    let count = 0
+    for(let i = 0; i<str.length; i++) {
+        if(str2[i] === 'x') {
+            count++
+        }
+    }
+    // while(true) {
+    //     let index = str.indexOf('x')
+    //     if(index !== -1) {
+    //         count++
+    //     } else {
+    //         str = str.slice(index + 1)
+    //         break
+    //     }
+    // }
+    return count
+}
+console.log(solution('1,1,0,0,1,1,1,0,1'))
+console.log(solution('1,0,1'))
