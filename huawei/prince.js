2c41d662845b91d961e11e73da2fc55402130f25
wanghh5 <wanghh5@baoneng.com>
2024-04-02 10:56:02 +0800

    提交树的深度和广度优先遍历

--- huawei/prince.js
+++ huawei/prince.js
@@ -2,7 +2,7 @@
  * @Author: wanghh
  * @Date: 2024-01-11 08:09:23
  * @LastEditors: wanghh
- * @LastEditTime: 2024-01-16 15:34:15
+ * @LastEditTime: 2024-04-02 10:31:12
  * @Description:
  */
 /** 寻宝
@@ -34,9 +34,11 @@ k 的取值范围如下：
 输出
 20
 
-解题思路： 
+解题思路： 树图的遍历问题 广度优先遍历 深度优先遍历
 双重循环 深度优先
  */
+
+// 深度优先 栈(后进先出) push pop 递归
 function solution(n, m, k) {
   let ans = 0;
   let offSets = [
@@ -45,7 +47,7 @@ function solution(n, m, k) {
     [-1, 0],
     [1, 0],
   ];
-  let visited = new Set();
+  let visited = new Set(); // 是否到达过
   //   let dp = new Array(n).fill(0).map((item) => new Array(m).fill(0));
   //   dp[0][0] = 1;
   function dfs(x, y) {
@@ -65,24 +67,59 @@ function solution(n, m, k) {
         dfs(newX, newY);
       }
     } else {
-      return ans;
+      return;
     }
   }
   dfs(0, 0);
   return ans;
 }
 
-// 记住一个知识点： 数位之和 就是个位+十位+百位等等 就是数位之和
+// 记住一个知识点： 数位之和 就是个位+十位+百位等等 就是数位之和 本题中最大就是50 所以求十位和个位就可以
 function getDigitSum(num) {
   let sum = 0;
   while (num > 0) {
-    console.log(num, "000");
+    // console.log(num, "000");
     sum += num % 10; // 个位数
     num = Math.floor(num / 10); // 十位数
-    console.log(num, "1111");
+    // console.log(num, "1111");
   }
   return sum;
 }
 
 console.log(getDigitSum(121));
-// console.log(solution(40, 40, 18));
+console.log(solution(40, 40, 18), "dfs");
+console.log(bfsSolution(40, 40, 18), "bfs");
+
+// 用广度优化的算法去做 队列（先进先出） unshift shift
+function bfsSolution(n, m, k) {
+  let ans = 0;
+  let offSets = [
+    [0, -1],
+    [0, 1],
+    [-1, 0],
+    [1, 0],
+  ];
+  let visited = new Set(); // 是否到达过
+  function bfs() {
+    let queue = [];
+    queue.push(0);
+    while (queue.length > 0) {
+      const pos = queue.shift(); // 取出第一个元素
+      let y = pos % n;
+      let x = (pos - y) / n;
+      for (let [offsetX, offsetY] of offSets) {
+        let newX = x + offsetX;
+        let newY = y + offsetY;
+        if (newX < 0 || newX >= m || newY < 0 || newY >= n) continue;
+        if (getDigitSum(newX) + getDigitSum(newY) > k) continue;
+        const newPos = newX * n + newY;
+        if (visited.has(newPos)) continue;
+        ans++;
+        visited.add(newPos);
+        queue.push(newPos);
+      }
+    }
+  }
+  bfs();
+  return ans;
+}
