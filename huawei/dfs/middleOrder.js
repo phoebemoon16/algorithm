9ff84fe64998b186eaf3b1e9a8387722f1bdf5ca
wanghh5 <wanghh5@baoneng.com>
2024-04-23 11:57:33 +0800

    提交中序遍历

--- huawei/dfs/middleOrder.js
+++ huawei/dfs/middleOrder.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: wanghh
+ * @Date: 2024-04-23 11:37:15
+ * @LastEditors: wanghh
+ * @LastEditTime: 2024-04-23 11:40:29
+ * @Description:
+ */
 /**
  * 二叉树的中序遍历
  * 
@@ -9,48 +16,48 @@
 中： 左 根 右
  */
 
-class TreeNode{
-    constructor(value, left=0, right = 0){
-        this.value = value
-        this.left = left
-        this.right = right
-    }
+class TreeNode {
+  constructor(value, left = 0, right = 0) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
 }
-
 
 //  将此数组转换为二叉树 记得for循环加2
 function buildTreeFromArray(arr) {
-    if (arr.length === 0) return null;
+  if (arr.length === 0) return null;
 
-    const queue = [];
-    const root = new TreeNode(arr[0]);
-    queue.push(root);
+  const queue = [];
+  const root = new TreeNode(arr[0]);
+  queue.push(root);
 
-    for (let i = 1; i < arr.length; i += 2) {
-        const current = queue.shift();
-        if (arr[i] !== null) {
-            current.left = new TreeNode(arr[i]);
-            queue.push(current.left);
-        }
-        if (i + 1 < arr.length && arr[i + 1] !== null) {
-            current.right = new TreeNode(arr[i + 1]);
-            queue.push(current.right);
-        }
+  // 分别循环增加左右节点
+  for (let i = 1; i < arr.length; i += 2) {
+    const current = queue.shift();
+    if (arr[i] !== null) {
+      current.left = new TreeNode(arr[i]);
+      queue.push(current.left);
     }
+    if (i + 1 < arr.length && arr[i + 1] !== null) {
+      current.right = new TreeNode(arr[i + 1]);
+      queue.push(current.right);
+    }
+  }
 
-    return root;
+  return root;
 }
 
-function solution(root){
-    let nodes =buildTreeFromArray(root)
-    let res = []
-    function inorder(node){
-        if(!node) return
-        inorder(node.left)
-        res.push(node.value)
-        inorder(node.right)
-    }
-    inorder(nodes)
-    return res
+function solution(root) {
+  let nodes = buildTreeFromArray(root);
+  let res = [];
+  function inorder(node) {
+    if (!node) return;
+    inorder(node.left);
+    res.push(node.value);
+    inorder(node.right);
+  }
+  inorder(nodes);
+  return res;
 }
-console.log(solution([1,null,2,3]))
+console.log(solution([1, null, 2, 3]));
