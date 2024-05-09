4c9530c2fdaa4d442b03448c9385d011b580e465
wanghh5 <wanghh5@baoneng.com>
2024-05-09 15:32:48 +0800

    提交滑动窗口最大值修改

--- letcode/window.js
+++ letcode/window.js
@@ -1,14 +1,7 @@
-/** 滑动窗口 
- * 
- * 
+/** 滑动窗口
  * https://leetcode.cn/problems/sliding-window-maximum/solutions/543818/ji-shu-shuang-duan-fen-kuai-you-xian-5xi-87ud/
- 三 分块
-将数组分成k块
-指针j → 统计每块内从块开头到j最大值
-指针i ← 统计每块内从块结尾到i最大值
-滑动区间[i, i + k - 1]最大值 = 某块结尾到i最大值 与 某块开头到i + k - 1最大值 取大
-
-*/
+ */
+// 此解法当队列的值超过一定数量时就会报错
 function maxSlidingWindow(nums, k) {
   let currentArray = [];
   if (nums.length < k) {
@@ -30,3 +23,49 @@ function maxSlidingWindow(nums, k) {
 }
 
 console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3));
+
+/**   
+ * 
+ * 就很像队列 ：一端进入 另一端移出 双向队列 以及 单调队列
+  当队首元素在原数组中的下标小于窗口左边界时，队首元素就需要移除队列。
+
+  设置一个队列 队列是当前窗口里的排序 从大到小排序
+  1.遍历数组中的元素 如果队列不为空且考察元素大于等于队尾元素，则将队尾元素移除。
+  2.当队首元素大于窗口左侧的index时，就要移除队首。
+  3.从数组0开始，当窗口右边界right+1大于等于窗口大小K时，意味着窗口行成，
+  此时，队首元素就是当前窗口的最大值 将最大值取出放在数组里即可。
+  */
+function solution2(nums, k) {
+  let queue = [0]; // 存储元素大小的下标
+  let res = [];
+  let right = 1;
+  // 当K长度为1 或者元素长度为1的时候 都直接但会nums
+  if (nums.length === 1 || k === 1) {
+    return nums;
+  }
+  while (right < nums.length) {
+    // if是有问题的 不能之和最后一个判断 要循环一下直到找到大于他无法修改的数组 这个判断是需要循环的 因为 -1 -2 5 新增的元素是比前2个都要大的元素
+    // if (queue.length > 0 && nums[right] > nums[queue[queue.length - 1]]) {
+    //   queue.pop();
+    // }
+    while (queue.length > 0 && nums[right] > nums[queue[queue.length - 1]]) {
+      queue.pop();
+    }
+    queue.push(right);
+    // 求出left 如果left是大于第一个元素的下标的话 就需要移除队列第一个元素
+    let left = right - k + 1;
+    if (queue[0] < left) {
+      queue.shift();
+      left++;
+    }
+    if (right + 1 >= k) {
+      // console.log(queue, "queuequeue");
+      res.push(nums[queue[0]]);
+    }
+    right++;
+  }
+  return res;
+}
+
+console.log(solution2([1, 3, -1, -3, 5, 3, 6, 7], 3));
+console.log(solution2([1], 1));
